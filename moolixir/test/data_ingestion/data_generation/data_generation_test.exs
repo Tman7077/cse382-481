cc3851d19b7a0ab999396198ee66e492a54cadbe
Tyler Bartle <tman.bartleboy1@gmail.com>
2025-03-20 19:36:24 -0600

    Removed unnecessary lines from the faux test module

--- moolixir/test/data_ingestion/data_generation/data_generation_test.exs
+++ moolixir/test/data_ingestion/data_generation/data_generation_test.exs
@@ -1,7 +1,5 @@
 defmodule DataGenTest do
-  alias DataIngestion.DataGeneration, as: DataGen
   use ExUnit.Case
-  doctest DataGen
 
   test "module DataIngestion.DataGeneration is a facade. No unit tests needed." do
     assert 0 == 0
